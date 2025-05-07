5208dada0594d92a6113a7dd80d0e147655a6cbf
Deepta Dutta <deeptaduttaksp@gmail.com>
2025-05-07 23:16:37 +0600

    added slider

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
       <div className="carousel w-full mx-auto rounded-lg">
         <div id="slide1" className="carousel-item relative w-full">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+            src="/Assets/data_file_1740467170_18618.jpg"
             className="w-full"
           />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
@@ -20,7 +20,7 @@ const Home = () => {
         </div>
         <div id="slide2" className="carousel-item relative w-full">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+            src="/public/Assets/InnovateX.png"
             className="w-full"
           />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
@@ -34,7 +34,7 @@ const Home = () => {
         </div>
         <div id="slide3" className="carousel-item relative w-full">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+            src="/Assets/DD-Disruptive-Doctors-Conference-exhibition-2025.png"
             className="w-full"
           />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
@@ -48,7 +48,7 @@ const Home = () => {
         </div>
         <div id="slide4" className="carousel-item relative w-full">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+            src="/Assets/ab171a_655a1a9eca744483ba3d194b8378c40d~mv2.png"
             className="w-full"
           />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
